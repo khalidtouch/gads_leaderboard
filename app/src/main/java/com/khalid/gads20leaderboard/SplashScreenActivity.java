66a6adc38f10eacc8693aa067a8587ea0b40a91b
khalidtouch <khalidhulabaloo9@gmail.com>
2020-09-05 00:56:02 +0200

    setup the flashscreen

---
+++ app/src/main/java/com/khalid/gads20leaderboard/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.khalid.gads20leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,
+                        HomeActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, 10000);
+    }
+}
