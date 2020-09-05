a4657e36ab7d1b596f9e78afeecb7fbbf88af747
khalidtouch <khalidhulabaloo9@gmail.com>
2020-09-05 03:19:16 +0200

    fixed the viewpager and tablayouts

--- app/src/main/java/com/khalid/gads20leaderboard/HomeActivity.java
+++ app/src/main/java/com/khalid/gads20leaderboard/HomeActivity.java
@@ -1,14 +1,27 @@
 package com.khalid.gads20leaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class HomeActivity extends AppCompatActivity {
+    public static String[] mChildFragmentNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+        mChildFragmentNames = new String[2];
+        mChildFragmentNames = getResources().getStringArray(R.array.child_fragment_names);
+
+        ViewPager pager = this.findViewById(R.id.main_pager);
+        TabLayout tabLayout = this.findViewById(R.id.tabLayout);
+
+        pager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(pager);
+
+        }
 }
